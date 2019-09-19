77affa85109cf2a4bb7d32cefd6165f81a917317
Hung <hungcao2112@gmail.com>
2019-09-19 23:41:46 +0700

    init

---
+++ HB-Partner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HB-Partner
+//
+//  Created by Hung Cao on 9/19/19.
+//  Copyright © 2019 Hung Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
