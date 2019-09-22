9b530ad8299588b6e9839d0afb22985ad49411c7
Hung <hungcao2112@gmail.com>
2019-09-22 20:58:23 +0700

    Update UI

---
+++ HB-Partner/ViewController/Share/WebViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WebViewController.swift
+//  HB-Partner
+//
+//  Created by Hung Cao on 9/22/19.
+//  Copyright © 2019 Hung Cao. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    private func setupWebView() {
+        webView.
+    }
+
+}
